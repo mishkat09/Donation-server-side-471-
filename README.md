18835f3d25f69a2037f08d2e717ab3cbd046aaab
Mishkat <mishkatsultana5738@gmail.com>
2023-11-18 02:49:34 +0600

    first commit

---
+++ README.md
@@ -0,0 +1,1 @@
+[LIVE_LINK](https://donation-server-side-1ab7az760-shahriaranuvab.vercel.app)
---
+++ index.js
@@ -0,0 +1,52 @@
+const express = require("express");
+const cors = require("cors");
+const app = express();
+const { MongoClient, ServerApiVersion } = require("mongodb");
+const port = process.env.PORT || 5000;
+
+app.use(cors());
+app.use(express.json());
+
+const uri =
+  "mongodb+srv://donation:AWYFDQirkSJE0X5p@cluster0.knrtjno.mongodb.net/?retryWrites=true&w=majority";
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
+});
+
+async function run() {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    await client.connect();
+
+    const donationData = client.db("donation").collection("donationData");
+
+    app.get("/data", async (req, res) => {
+      const cursor = donationData.find();
+      const result = await cursor.toArray();
+      res.send(result);
+    });
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!"
+    );
+  } finally {
+    // Ensures that the client will close when you finish/error
+    // await client.close();
+  }
+}
+run().catch(console.dir);
+
+app.get("/", (req, res) => {
+  res.send("Donation Server Is Running");
+});
+
+app.listen(port, () => {
+  console.log(`Donation Server Is Running On PORT: ${port}`);
+});
